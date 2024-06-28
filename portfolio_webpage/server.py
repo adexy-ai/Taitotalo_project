940cd8e80ecc30dffa7a1172b4a6fb994e3b2c4d
Anuoluwapo ADENIRAN <69057307+adexy-ai@users.noreply.github.com>
2024-06-28 13:42:28 +0300

    completed the contact page and setting up the ml project page

--- portfolio_webpage/server.py
+++ portfolio_webpage/server.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
 import datetime as dt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import webview
 
 
 app = Flask(__name__)
+
+# Sample data for training the model
+data = {
+    'population': [1000, 2000, 3000, 4000, 5000],
+    'diabetes_cases': [50, 100, 150, 200, 250]
+}
+
+df = pd.DataFrame(data)
+
+X = df[['population']]
+y = df['diabetes_cases']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    population = int(request.form['population'])
+    prediction = model.predict(np.array([[population]]))[0]
+    return jsonify({'predicted_diabetes_cases': prediction})
 
 @app.route("/")
 def home_page():
@@ -27,7 +54,6 @@ def generic():
 
 
 
-
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    
