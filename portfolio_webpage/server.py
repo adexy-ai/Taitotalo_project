7f54c127334630441c0a4ff765600605893f9ee0
Anuoluwapo ADENIRAN <69057307+adexy-ai@users.noreply.github.com>
2024-07-05 18:39:20 +0300

    added and listed databases

--- portfolio_webpage/server.py
+++ portfolio_webpage/server.py
@@ -1,13 +1,29 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import numpy as np
 import pandas as pd
 import datetime as dt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import webview
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL, Email, Length
+import csv
 
 
-app = Flask(__name__)
+app = Flask(__name__) 
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# Creating the form function to get the import from the form page of the website
+
+class Message_Ade(FlaskForm):
+    name = StringField('NAME', validators=[DataRequired()])
+    email = StringField('EMAIL', validators=[DataRequired(), Email()])
+    message = StringField('MESSAGE', validators=[DataRequired(), Length(min=2, max=150)])
+    submit = SubmitField('SEND MESSAGE')
+
 
 # Sample data for training the model
 data = {
@@ -40,16 +56,26 @@ def home_page():
 def elements():
     return render_template('projects.html')
 
-@app.route("/contacts")
+@app.route("/contacts", methods=['POST', 'GET'])
 def generic():
-    return render_template('contacts.html')
+    form = Message_Ade()
+    if form.validate_on_submit():
+        with open("messages.csv", mode="a", encoding='utf-8') as csv_file:
+            csv_file.write(f"\n{form.name.data},"
+                           f"{form.email.data},)
+                           f"{form.message.data},") 
+    return render_template('contacts.html', form=form)
 
 @app.route("/form_entry", methods=['POST', 'GET'])
 def recieve_data():
-    name = request.form.get['name']
-    email = request.form.get['email']
-    message = request.form.get['message']
-    return f'<h2>Name: {name}, Email: {email}, Message: {message}<h2/>'
+    form = Message_Ade()
+    if form.validate_on_submit():
+        with open("messages.csv", mode="a", encoding='utf-8') as csv_file:
+            csv_file.write(f"\n{form.name.data},"
+                           f"{form.email.data},"
+                           f"{form.message.data},")  
+        return render_template('footer.html', form=form)
+
 
 
 
